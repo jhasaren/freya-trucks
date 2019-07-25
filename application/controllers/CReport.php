81a01fc3930c1af5cb8fc4b5062eb82a7576621e
John A. Sanchez R <37158730+jhasaren@users.noreply.github.com>
2019-07-25 14:26:07 -0500

    Ajuste Reimpresion de Ticket
    En el modulo de reportes se ajusto la reimpresion del ticket para que mantenga al usuario en la misma pantalla cuando haga clic en reimprimir.

--- application/controllers/CReport.php
+++ application/controllers/CReport.php
@@ -429,7 +429,9 @@ class CReport extends CI_Controller {
                 log_message('DEBUG', 'Reimprime Factura');
                 /*Invoca funcion de Mike42_Helper*/
                 escposticket($detailRecibo,$this->session->userdata('nombre_sede'),$this->session->userdata('dir_sede'),$this->session->userdata('printer_sede'),$turno,$nitRecibo);
-                //$this->liquidasale();    
+
+                /*Redirecciona al detalle del recibo*/
+                $this->detallerecibo($detailRecibo['general']->idVenta,$detailRecibo['general']->nroRecibo);
                 
             } else {
                 
