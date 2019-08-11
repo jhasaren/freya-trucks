553f68fd5f8e18493a71af5c7173569fcaf8c0a2
John Alexander Sanchez Rengifo <37158730+jhasaren@users.noreply.github.com>
2019-08-11 10:39:17 -0500

    Ajuste redondeo valor servicio

--- public/gentelella/build/js/custom.js
+++ public/gentelella/build/js/custom.js
@@ -189,20 +189,20 @@ $(document).ready(function () {
 $(document).ready(function () {
     var subtotal = $('#subtotal_venta').val();
     var porcenServ = $('#porcen_servicio').val();
-    $("#value_servicio").val(subtotal*(porcenServ/100));
+    $("#value_servicio").val((subtotal*(porcenServ/100)));
     
     console.log('subtotal ->'+subtotal);
     console.log('procentaje ->'+porcenServ);
     
     $("#value_servicio").blur(function() {
         var valueServ = $('#value_servicio').val();
-        $("#porcen_servicio").val((valueServ/subtotal)*100);
+        $("#porcen_servicio").val(((valueServ/subtotal)*100));
         console.log('valor ->'+valueServ);
     });
     
     $("#porcen_servicio").blur(function() {
         var porcenServ = $('#porcen_servicio').val();
-        $("#value_servicio").val(subtotal*(porcenServ/100));
+        $("#value_servicio").val((subtotal*(porcenServ/100)));
         console.log('porcentaje ->'+porcenServ);
     });
 });
